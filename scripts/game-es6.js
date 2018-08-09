03492b997f97bf7ed60c6c8c65241f503c2b99fd
@AphroditeF <a.flora@outlook.com>
2018-08-10 00:43:29 +0300

    Sets up the Canvas with CreateJS

--- scripts/game-es6.js
+++ scripts/game-es6.js
@@ -1,8 +1,23 @@
 //Logic for the Count Game
 
 class Game{
-    constructor (){
+    constructor(){
         console.log(`Welcome to the game. Version ${this.version()}`);
+    
+        this.canvas=document.getElementById("game-canvas");
+        this.stage=new createjs.Stage(this.canvas);
+
+        createjs.Ticker.setFPS(60);
+        
+        //Keep re-drawing the stage.
+        createjs.Ticker.on("tick", this.stage);
+
+        //Testing code
+        var circle= new createjs.Shape();
+        circle.graphics.beginFill("red").drawCircle(0,0,40);
+        circle.x=circle.y=100;
+        this.stage.addChild(circle);
+
     }
     version(){
         return '1.0.0';
